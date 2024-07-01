1ae5e28b82eef0cd0b151c7e3da507481c447d3b
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-07-01 15:26:18 +0100

    Add margin to logo container

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -2,18 +2,19 @@ import React from 'react';
 import styles from '../styles/Logo.module.css';
 import LogoImg from '../assets/logo.webp';
 
-// Logo component to render logo
-function Logo() {
+// Logo component to render logo which will disappear on scroll
+const Logo = () => {
   return (
-    <div className={styles.LogoContainer}>
+    <div className={`${styles.LogoContainer} mb-4`}>
         <img 
             className={styles.Logo} 
             src={LogoImg} 
             alt="Preloved Canine Clobber Logo" 
         />
-    </div>    
+    </div>  
   )
 };
 
 export default Logo;
+
 
