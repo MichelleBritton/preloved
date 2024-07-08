b989912d633b320dfab13d9062da390c0ad34c97
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-07-08 15:47:13 +0100

    Create three search boxes for home page

--- frontend/src/pages/adverts/AdvertsPage.js
+++ frontend/src/pages/adverts/AdvertsPage.js
@@ -8,6 +8,7 @@ import NoResults from "../../assets/no-results.png";
 
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 import { axiosReq } from "../../api/axiosDefaults";
@@ -20,12 +21,15 @@ function AdvertsPage({ message }) {
     const [hasLoaded, setHasLoaded] = useState(false);
     // Detect any url change
     const { pathname } = useLocation();
-    const [query, setQuery] = useState("");
+    const [queryKey, setQueryKey] = useState("");
+    const [queryCat, setQueryCat] = useState("");
+    const [queryLoc, setQueryLoc] = useState("");
 
     useEffect(() => {
         const fetchAdverts = async () => {
             try {
-                const { data } = await axiosReq.get(`/adverts/?search=${query}`);
+                const queryString = `?search=${queryKey}&category=${queryCat}&location=${queryLoc}`;
+                const { data } = await axiosReq.get(`/adverts/${queryString}`);
                 setAdverts(data);
                 setHasLoaded(true);
             } catch(err) {
@@ -42,25 +46,66 @@ function AdvertsPage({ message }) {
             clearTimeout(timer);
         };
 
-    }, [query, pathname]);
+    }, [queryKey, queryCat, queryLoc, pathname]);
   
     return (
         <>
-            <Container fluid className={`${appStyles.Content} custom-container`}>
-                <i className={`fas fa-search ${styles.SearchIcon}`} />
-                <Form 
-                    className={styles.SearchBar} 
-                    onSubmit={(event) => event.preventDefault()}
-                >
-                    <Form.Control 
-                        value={query} 
-                        onChange={(event) => setQuery(event.target.value)} 
-                        type="text" 
-                        className="mr-sm-2" 
-                        placeholder="Search Adverts"
-                        aria-label="Search" 
-                    />
-                </Form>    
+            <Container fluid className="custom-container d-flex justify-content-between">
+                <Row>
+                    <Col className={appStyles.ContentDark}>
+                        <h2>Search by keyword</h2>
+                        <i className={`fas fa-search ${styles.SearchIcon}`} />
+                        <Form 
+                            className={styles.SearchBar} 
+                            onSubmit={(event) => event.preventDefault()}
+                        >
+                            <Form.Control 
+                                value={queryKey} 
+                                onChange={(event) => setQueryKey(event.target.value)} 
+                                type="text" 
+                                className="mr-sm-2" 
+                                placeholder="What are you looking for?"
+                                aria-label="Search" 
+                            />
+                        </Form>    
+                    </Col>
+
+                    <Col className={appStyles.ContentDark}>
+                        <h2>Search by category</h2>
+                        <i className={`fas fa-search ${styles.SearchIcon}`} />
+                        <Form 
+                            className={styles.SearchBar} 
+                            onSubmit={(event) => event.preventDefault()}
+                        >
+                            <Form.Control 
+                                value={queryCat} 
+                                onChange={(event) => setQueryCat(event.target.value)} 
+                                type="text" 
+                                className="mr-sm-2" 
+                                placeholder="Category"
+                                aria-label="Search" 
+                            />
+                        </Form>    
+                    </Col>
+
+                    <Col className={appStyles.ContentDark}>
+                        <h2>Search by location</h2>
+                        <i className={`fas fa-search ${styles.SearchIcon}`} />
+                        <Form 
+                            className={styles.SearchBar} 
+                            onSubmit={(event) => event.preventDefault()}
+                        >
+                            <Form.Control 
+                                value={queryLoc} 
+                                onChange={(event) => setQueryLoc(event.target.value)} 
+                                type="text" 
+                                className="mr-sm-2" 
+                                placeholder="Location"
+                                aria-label="Search" 
+                            />
+                        </Form>    
+                    </Col>
+                </Row>                
             </Container>
 
             <Container fluid className={`${appStyles.Content} custom-container`}>
