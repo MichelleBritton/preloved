32f603a8e88d29947241081f90b67dd9a28a30ce
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-07-09 18:28:28 +0100

    Re-add search functionality to include select field instead of text field

--- frontend/src/pages/adverts/AdvertsPage.js
+++ frontend/src/pages/adverts/AdvertsPage.js
@@ -15,18 +15,22 @@ import { axiosReq } from "../../api/axiosDefaults";
 import { fetchMoreData } from "../../utils/utils";
 import Advert from "./Advert";
 import Asset from "../../components/Asset";
+import useSelectOptions from '../../hooks/useSelectOptions';
 
 function AdvertsPage({ message }) {
     const [adverts, setAdverts] = useState({ results: []});
     const [hasLoaded, setHasLoaded] = useState(false);
     // Detect any url change
     const { pathname } = useLocation();
-    const [query, setQuery] = useState("");
+    const [queryKey, setQueryKey] = useState("");
+    const [queryCat, setQueryCat] = useState("");
+    const [queryLoc, setQueryLoc] = useState("");
+    const { categoryOptions, locationOptions } = useSelectOptions();
 
     useEffect(() => {
         const fetchAdverts = async () => {
             try {
-                const { data } = await axiosReq.get(`/adverts/?search=${query}`);
+                const { data } = await axiosReq.get(`/adverts/?search=${queryCat}`);
                 setAdverts(data);
                 setHasLoaded(true);
             } catch(err) {
@@ -42,7 +46,7 @@ function AdvertsPage({ message }) {
             clearTimeout(timer);
         };
 
-    }, [query, pathname]);
+    }, [queryCat, pathname]);
   
     return (
         <>
@@ -56,8 +60,8 @@ function AdvertsPage({ message }) {
                             onSubmit={(event) => event.preventDefault()}
                         >
                             <Form.Control 
-                                value={query} 
-                                onChange={(event) => setQuery(event.target.value)} 
+                                value={queryKey} 
+                                onChange={(event) => setQueryKey(event.target.value)} 
                                 type="text" 
                                 className="mr-sm-2" 
                                 placeholder="What are you looking for?"
@@ -66,41 +70,54 @@ function AdvertsPage({ message }) {
                         </Form>    
                     </Col>
 
-                    {/* <Col className={appStyles.ContentDark}>
+                    <Col className={appStyles.ContentDark}>
                         <h2>Search by category</h2>
-                        <i className={`fas fa-search ${styles.SearchIcon}`} />
                         <Form 
                             className={styles.SearchBar} 
                             onSubmit={(event) => event.preventDefault()}
                         >
-                            <Form.Control 
-                                value={queryCat} 
-                                onChange={(event) => setQueryCat(event.target.value)} 
-                                type="text" 
-                                className="mr-sm-2" 
-                                placeholder="Category"
-                                aria-label="Search" 
-                            />
+                            <Form.Control
+                                as="select"
+                                value={queryCat}
+                                onChange={(event) => setQueryCat(event.target.value)}
+                                name="category"
+                                className="mr-sm-2"
+                                aria-label="Search category"
+                            >
+                                <option>Please select</option>
+                                {categoryOptions.map(option => (
+                                    <option key={option.value} value={option.value}>
+                                        {option.label}
+                                    </option>
+                                ))}
+                            </Form.Control>
                         </Form>    
-                    </Col> */}
+                    </Col>
 
-                    {/* <Col className={appStyles.ContentDark}>
+                    <Col className={appStyles.ContentDark}>
                         <h2>Search by location</h2>
-                        <i className={`fas fa-search ${styles.SearchIcon}`} />
                         <Form 
                             className={styles.SearchBar} 
                             onSubmit={(event) => event.preventDefault()}
                         >
-                            <Form.Control 
-                                value={queryLoc} 
-                                onChange={(event) => setQueryLoc(event.target.value)} 
-                                type="text" 
+                            <Form.Control
+                                as="select"
+                                value={queryLoc}
+                                onChange={(event) => setQueryLoc(event.target.value)}
+                                name="location"
                                 className="mr-sm-2" 
-                                placeholder="Location"
-                                aria-label="Search" 
-                            />
+                                aria-label="Search location" 
+                            >
+                                <option>Please select</option>
+                                {locationOptions.map(option => (
+                                    <option key={option.value}>
+                                        {option.label}
+                                    </option>
+                                ))}
+                            </Form.Control>
                         </Form>    
-                    </Col> */}
+
+                    </Col>
                 </Row>                
             </Container>
 
